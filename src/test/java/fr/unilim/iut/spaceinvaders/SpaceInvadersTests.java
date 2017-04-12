122cdbc98ffd42e59e3ff069edc8fb94d35055ba
Youenn <youenn.droal@etu.unilim.fr>
2017-04-12 11:45:00 +0200

    3eme commit

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTests.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTests.java
@@ -19,6 +19,24 @@
 		    "...............\n" + 
 		    "...............\n" + 
 		    "...............\n" , spaceInvaders.toString());
+		    
 	        }
+		@Test
+		public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+			SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+			spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+			assertEquals("" + 
+			"...............\n" + 
+			"...............\n" +
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			"...............\n" + 
+			".......V.......\n" , spaceinvaders.toString());
+			
+		}
 
        }
